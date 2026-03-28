5848fe8f8d0679ec99186dd10832d141b03dbd38
onlypasha7 <onlypasha7@outlook.co.id>
2026-03-28 15:13:03 +0700

    add title for MainForm.cs

--- ManagementPerpustakaan/Layouts/MainForm.cs
+++ ManagementPerpustakaan/Layouts/MainForm.cs
@@ -6,6 +6,7 @@ public partial class MainForm : Form
     public MainForm()
     {
         InitializeComponent();
+        this.Text = "Management Perpustakaan";
     }
 
     private void TampilkanHalaman(System.Windows.Forms.UserControl uc)
